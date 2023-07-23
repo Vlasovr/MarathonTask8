d399503f3b8047951753ed7726cecba37d2a8202
Роман Власов <78149386+Vlasovr@users.noreply.github.com>
2023-07-23 23:39:54 +0300

    final commit

--- NativeAvatar/ViewController.swift
+++ NativeAvatar/ViewController.swift
@@ -1,51 +1,51 @@
 import UIKit
 
-final class ViewController: UIViewController, UIScrollViewDelegate {
+final class ViewController: UIViewController {
     
     private lazy var scrollView: UIScrollView = {
         let sv = UIScrollView()
         sv.contentSize = CGSize(width: view.bounds.width, height: 1500)
-        sv.delegate = self
-        sv.backgroundColor = .orange
+        sv.backgroundColor = .purple
         return sv
     }()
     
-    private lazy var imageView: UIImageView = {
-        let iv = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
-        iv.clipsToBounds = true
-        return iv
+    private lazy var avatarImage: UIImageView = {
+        let av = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
+        av.tintColor = .systemGray
+        av.clipsToBounds = true
+        return av
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view = scrollView
         setupUI()
+        
     }
     
     private func setupUI(){
         title = "Avatar"
-        guard let navigationBar = self.navigationController?.navigationBar else { return }
-        navigationBar.addSubview(imageView)
         
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            imageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: -20),
-            imageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -8),
-            imageView.heightAnchor.constraint(equalToConstant: 36),
-            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
-        ])
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let window = UIApplication.shared.windows.first
-        let topPadding = window?.safeAreaInsets.top ?? 0
-        let offsetY = scrollView.contentOffset.y
-        print(offsetY)
-        if offsetY >= -149.6 {
-            imageView.transform = .init(translationX: 0, y: offsetY)
-        } else {
-            imageView.transform = .init(translationX: 0, y: 36)
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        
+        for subview in navigationBar.subviews {
+            if NSStringFromClass(subview.classForCoder).contains("UINavigationBarLargeTitleView") {
+                
+                subview.addSubview(avatarImage)
+                
+                avatarImage.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    avatarImage.rightAnchor.constraint(equalTo: subview.rightAnchor,
+                                                       constant: -20),
+                    avatarImage.topAnchor.constraint(equalTo: subview.bottomAnchor, constant: 20),
+                    avatarImage.bottomAnchor.constraint(equalTo: avatarImage.superview?.bottomAnchor ?? navigationBar.bottomAnchor, constant: -10),
+                    avatarImage.heightAnchor.constraint(equalToConstant: 36),
+                    avatarImage.widthAnchor.constraint(equalToConstant: 36)
+                ])
+            }
         }
     }
 }
+
+
 
